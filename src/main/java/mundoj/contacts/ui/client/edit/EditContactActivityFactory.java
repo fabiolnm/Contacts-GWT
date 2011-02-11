0f739c9c12b3172aa287da3802a4a57a46ae99f5
Fabio Luiz Nery de Miranda <fabio@miranti.net.br>
2011-02-11 10:41:16 -0200

    - As Atividades passaram a ser refenciadas apenas pelas suas respectivas fábricas. - Faz sentido movê-las para dentro das fábricas, como inner classes. - Com isso, o código das atividades fica mais simples, pois poder acessar diretamente objetos que de outra forma a Fábrica teria que passar no construtor da atividade.

--- src/main/java/mundoj/contacts/ui/client/edit/EditContactActivityFactory.java
+++ src/main/java/mundoj/contacts/ui/client/edit/EditContactActivityFactory.java
@@ -1,10 +1,14 @@
 package mundoj.contacts.ui.client.edit;
 
+import com.google.gwt.activity.shared.AbstractActivity;
 import com.google.gwt.activity.shared.Activity;
 import com.google.gwt.event.shared.EventBus;
+import com.google.gwt.user.client.Timer;
+import com.google.gwt.user.client.ui.AcceptsOneWidget;
 import com.google.inject.Inject;
 
 import mundoj.contacts.client.AbstractActivityFactory;
+import mundoj.contacts.domain.Contact;
 
 public class EditContactActivityFactory extends AbstractActivityFactory<EditContactPlace> {
 	private final EditContactView editor;
@@ -17,6 +21,34 @@ public class EditContactActivityFactory extends AbstractActivityFactory<EditCont
 
 	@Override
 	protected Activity create(EventBus eventBus, EditContactPlace place) {
-		return new EditContactActivity(editor, place);
+		return new EditContactActivity(place);
+	}
+	
+	public class EditContactActivity extends AbstractActivity {
+		private final EditContactPlace place;
+
+		public EditContactActivity(EditContactPlace place) {
+			this.place = place;
+		}
+
+		@Override
+		public void start(final AcceptsOneWidget panel, EventBus eventBus) {
+			panel.setWidget(editor);
+			editContact();
+		}
+
+		private void editContact() {
+			editor.startLoading("Carregando dados do usuário");
+			// emula carregamento dos dados de um usuário, para edição
+			new Timer() {
+				public void run() {	
+					editor.bind(Contact.get(place.contactId));	
+				}
+			}.schedule(1000);
+		}
+
+		public String mayStop() {
+			return editor.hasChanges() ? "Descartar Alterações?" : null;
+		}
 	}
 }
--- src/main/java/mundoj/contacts/ui/client/search/SearchContactsActivityFactory.java
+++ src/main/java/mundoj/contacts/ui/client/search/SearchContactsActivityFactory.java
@@ -1,10 +1,16 @@
 package mundoj.contacts.ui.client.search;
 
+import java.util.List;
+
+import com.google.gwt.activity.shared.AbstractActivity;
 import com.google.gwt.activity.shared.Activity;
 import com.google.gwt.event.shared.EventBus;
+import com.google.gwt.user.client.Timer;
+import com.google.gwt.user.client.ui.AcceptsOneWidget;
 import com.google.inject.Inject;
 
 import mundoj.contacts.client.AbstractActivityFactory;
+import mundoj.contacts.domain.Contact;
 
 public class SearchContactsActivityFactory extends AbstractActivityFactory<SearchContactsPlace> {
 	private final SearchContactsView view;
@@ -17,6 +23,44 @@ public class SearchContactsActivityFactory extends AbstractActivityFactory<Searc
 
 	@Override
 	protected Activity create(EventBus eventBus, SearchContactsPlace place) {
-		return new SearchContactsActivity(view, place);
+		return new SearchContactsActivity(place);
+	}
+	
+	public class SearchContactsActivity extends AbstractActivity {
+		private final SearchContactsPlace place;
+		private Timer currentTimer;
+
+		public SearchContactsActivity(SearchContactsPlace place) {
+			this.place = place;
+		}
+
+		@Override
+		public void start(final AcceptsOneWidget panel, EventBus eventBus) {
+			panel.setWidget(view);
+			view.startLoading(place.keyword);
+			searchContacts();
+		}
+
+		private void searchContacts() {
+			// timer = emula delay em uma chamada cliente-servidor
+			currentTimer = new Timer() {
+				@Override
+				public void run() {
+					List<Contact> contacts = Contact.filter(place.keyword);
+					view.update(contacts);
+				}
+			};
+			currentTimer.schedule(3000);
+		}
+
+		@Override
+		public String mayStop() {
+			return view.isLoading() ? "Deseja interromper a pesquisa em andamento?" : null;
+		}
+
+		@Override
+		public void onStop() {
+			currentTimer.cancel();
+		}
 	}
 }
